fc82eed0de79bd23438417b9ec00a9d29d26b821
Oliver Eriksson <oeriksson854@gmail.com>
2024-12-02 22:32:48 +0100

    6/7 tests work

--- server.test.js
+++ server.test.js
@@ -13,22 +13,21 @@ describe("API tests", function() {
             useUnifiedTopology: true
         })
     })
-    afterAll(async function() { //after everything delete temporary products and close connections
-        await Product.deleteMany({});
+    afterAll(async function() { //after everything close connections
         mongoose.connection.close();
         httpServer.close();
     });
     test("GET /api/products should return an array of products", async function() {
         await Product.create([
             { name: "Test Product", description: "Test", price: 100, quantity: 5, category: "Test", },
-            { name: "Test Product2", description: "Test2", price: 102, quantity: 2, category: "Test2" }
-        ])
+            { name: "Test Product2", description: "Test2", price: 102, quantity: 2, category: "Test2", }
+        ]);
         const response = await request(httpServer).get("/api/products");
         expect(response.status).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
     });
     test("GET /api/products/:id should return a product based off ID", async function() {
-        const newProduct = { name: "Test Product3", description: "Test3", price: 103, quantity: 3, category: "Test3" };
+        const newProduct = { name: "Test Product3", description: "Test3", price: 103, quantity: 3, category: "Test3", };
         const response = await request(httpServer).post("/api/products").send(newProduct);
         expect(response.status).toBe(200);
 
@@ -42,38 +41,33 @@ describe("API tests", function() {
         const response = await request(httpServer).get("/api/products?name=ps");
     })
     test("POST /api/products should post a new product", async function() { //not done
-        const newProduct = { name: "heyhey", description: "hey3", price: 666, quantity: 99, category: "heyhey" };
+        const newProduct = { name: "heyhey", description: "hey3", price: 666, quantity: 99, category: "heyhey", };
 
         const response = await request(httpServer).post("/api/products").send(newProduct);
         expect(response.status).toBe(200);
         expect(response.body.name).toBe(newProduct.name);
 
-        const verifyDatab = await Product.findById(response.body._id);
-        expect(verifyDatab).not.toBeNull();
-        expect(verifyDatab.name).toBe(newProduct.name);
+        const verifyData = await Product.findById(response.body._id);
+        expect(verifyData).not.toBeNull();
+        expect(verifyData.name).toBe(newProduct.name);
     })
     test("DELETE /api/products/:id should delete a product based off ID", async function() { 
-        const product = await Product.create({ name: "Test Product6", description: "Test6", price: 109, quantity: 6, category: "Test6", })
-        const response = await request(httpServer).post("/api/products").send({
-            name: product.name,
-            description: product.description,
-            price: product.price,
-            quantity: product.quantity,
-            category: product.category,
-        });
+        const newProduct = { name: "Test Product6", description: "Test6", price: 109, quantity: 6, category: "Test6", }
+        const response = await request(httpServer).post("/api/products").send(newProduct);
         expect(response.status).toBe(200);
+        const productId = response.body._id;
 
-        const deleteProd = await request(httpServer).delete(`/api/products/${product._id}`);
+        const deleteProd = await request(httpServer).delete(`/api/products/${productId}`);
         expect(deleteProd.status).toBe(200);
 
-        const deleteConfirm = await request(httpServer).get(`/api/products/${product._id}`);
+        const deleteConfirm = await request(httpServer).get(`/api/products/${productId}`);
         expect(deleteConfirm.status).toBe(404);
     })
     test("DELETE /api/products should delete all products", async function() {
         await Product.create([
-            { name: "Test Product4", description: "Test4", price: 106, quantity: 4, category: "Test4", },
+            { name: "Test Product4", description: "Test4", price: 106, quantity: 4, category: "Test4" },
             { name: "Test Product5", description: "Test5", price: 107, quantity: 5, category: "Test5" }
-        ])
+        ]);
         const deleteProds = await request(httpServer).delete("/api/products");
         expect(deleteProds.status).toBe(200);
 
